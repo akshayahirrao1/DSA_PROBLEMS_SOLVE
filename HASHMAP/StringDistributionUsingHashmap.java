b183367cbdaeb0f6d9bc7989989086aebea233a2
Akshay Ahirrao <akshayahirrao103@gmail.com>
2026-07-03 22:00:03 +0530

    learned Hashset and solved Evenodd and StringDistribution problems using hashmap

---
+++ HASHMAP/StringDistributionUsingHashmap.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+class StringDistributionUsingHashmap{
+    public static void main(String[] args){
+        String[] words = {"apple","ant","banana","bat","ball","cat","car"};
+        HashMap<Character, ArrayList<String>> map = new HashMap<>();
+        for(String word :words){
+            char firstchar = word.charAt(0);
+            if(!map.containsKey(firstchar)){
+                map.put(firstchar, new ArrayList<>());
+            }
+            map.get(firstchar).add(word);
+        }
+        System.out.println(map);
+    }
+}
