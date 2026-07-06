383795c0e52184859b33684414a977bc32e2d5df
Akshay Ahirrao <akshayahirrao103@gmail.com>
2026-07-06 13:46:28 +0530

    added StringDistributionUsingHashmap file

--- HASHMAP/StringDistributionUsingHashmap.java
+++ HASHMAP/StringDistributionUsingHashmap.java
@@ -1,12 +1,15 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
-class StringDistributionUsingHashmap{
-    public static void main(String[] args){
-        String[] words = {"apple","ant","banana","bat","ball","cat","car"};
+
+class StringDistributionUsingHashmap {
+
+    public static void main(String[] args) {
+        String[] words = {"apple", "ant", "banana", "bat", "ball", "cat", "car"};
         HashMap<Character, ArrayList<String>> map = new HashMap<>();
-        for(String word :words){
+        for (String word : words) {
             char firstchar = word.charAt(0);
-            if(!map.containsKey(firstchar)){
+            if (!map.containsKey(firstchar)) {
                 map.put(firstchar, new ArrayList<>());
             }
             map.get(firstchar).add(word);
