554c4190d47f6e63fdd2e7c7117b91803f2ebddd
Akshay Ahirrao <akshayahirrao103@gmail.com>
2026-07-18 17:24:20 +0530

    learned variable size sliding window and solved

---
+++ SLIDING_WINDOW/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class LongestSubstringWithoutRepeatingCharacters{
+    public static void main(String[] args){
+        HashSet<Character> set = new HashSet<>();
+        int maxlength = 0;
+        int left =0;
+        String s ="abcabces";
+        for( int right =0; right <s.length();right++){
+            char ch = s.charAt(right);
+            while(set.contains(ch)){
+                char leftchar = s.charAt(left);
+                set.remove(leftchar);
+                left++;
+            }
+            set.add(ch);
+            maxlength = Math.max(maxlength,right-left+1);
+        }
+       System.out.println(maxlength);
+
+    }
+}
