f015da1dae133049f9df06f00db9ae0fae2ed760
Akshay Ahirrao <akshayahirrao103@gmail.com>
2026-07-28 18:17:48 +0530

    learned monotonic stack, solved finding next greater element

---
+++ STACK/NextGreaterElement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class NextGreaterElement{
+    public static void main(String[] args){
+        int[] arr = {2,4,3,9,5,8};
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = arr.length-1 ; i >= 0 ; i++){
+            while(!st.isEmpty() && st.peek() <= arr[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }
+            else
+            {
+                ans[i] = st.peek();
+            }
+
+            st.push(arr[i]);
+        }
+
+       System.out.println(Arrays.toString(ans));
+    }
+}
